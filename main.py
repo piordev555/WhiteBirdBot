eecc44b7b4ad4395882147c091324d7798db2ee2
Professional Developer <hakus.superdev@gmail.com>
2022-07-01 01:30:37 +0900

    updated main.py
    updated main.py. bot command fetching method

--- main.py
+++ main.py
@@ -44,14 +44,14 @@ def geeks_url(update: Update, context: CallbackContext):
 		"GeeksforGeeks URL => https://www.geeksforgeeks.org/")
 
 
-def unknown(update: Update, context: CallbackContext):
-	update.message.reply_text(
-		"Sorry '%s' is not a valid command" % update.message.text)
+# def unknown(update: Update, context: CallbackContext):
+# 	update.message.reply_text(
+# 		"Sorry '%s' is not a valid command" % update.message.text)
 
 
-def unknown_text(update: Update, context: CallbackContext):
-	update.message.reply_text(
-		"Sorry I can't recognize you , you said '%s'" % update.message.text)
+# def unknown_text(update: Update, context: CallbackContext):
+# 	update.message.reply_text(
+# 		"Sorry I can't recognize you , you said '%s'" % update.message.text)
 
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
